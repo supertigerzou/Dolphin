a38d3ef555f9eec409ceb213eababdbb712cbd4c
supertigerzou <supertigerzou@gmail.com>
2013-03-16 23:11:29 +0800

    troop service

---
+++ src/Presentation/Future.Web(Troop)/Query/CountryResourceLoader.cs
@@ -0,0 +1,22 @@
+﻿using EF.Troop;
+using EF.Troop.Query;
+using Future.Web_Troop_.DataContracts;
+using System.Collections.Generic;
+
+namespace Future.Web_Troop_.Query
+{
+    public class CountryResourceLoader : IResourceLoader
+    {
+        public IEnumerable<IResource> Load(IResourceServiceContext context, IEnumerable<string> codes)
+        {
+            var results = new List<Country>();
+
+            foreach (var code in codes)
+            {
+                results.Add(new Country(code) { Name = "Country_" + code });
+            }
+
+            return results;
+        }
+    }
+}
